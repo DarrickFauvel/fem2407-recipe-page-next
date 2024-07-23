83b8912c5c365ed66199e0c9241c928b9a136df4
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-07-23 04:47:34 -0400

    Creat title component

---
+++ app/components/recipe/components/title-level2/index.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react"
+
+type RecipeTitleProps = {
+  children: ReactNode
+}
+
+const RecipeTitleLevel2 = ({ children }: RecipeTitleProps) => {
+  return (
+    <h3 className="text-fem-brown-800 text-3xl font-young-serif leading-none">
+      {children}
+    </h3>
+  )
+}
+export default RecipeTitleLevel2
