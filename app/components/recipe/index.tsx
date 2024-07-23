e4e3c15d9234d45af0b2722ff65ac5d947a2ae70
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-07-23 10:10:06 -0400

    Adjust paddings

--- app/components/recipe/index.tsx
+++ app/components/recipe/index.tsx
@@ -12,7 +12,7 @@ const Recipe = () => {
         <RecipeImage />
       </div>
       <div className="flex flex-col gap-8 px-8 py-10">
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-6">
           <RecipeTitle>Simple Omelette Recipe</RecipeTitle>
           <p>
             An easy and quick dish, perfect for any meal. This classic omelette
@@ -22,19 +22,20 @@ const Recipe = () => {
         </div>
 
         <RecipePreparation />
+
         <table>
           <tr className="border-t-0 border-r-0 border-l-0 border-b-[1px] border-b-fem-stone-150">
-            <td className="py-6">
+            <td className="pb-8">
               <RecipeIngredients />
             </td>
           </tr>
           <tr className="border-t-0 border-r-0 border-l-0 border-b-[1px]">
-            <td className="py-6">
+            <td className="py-8">
               <RecipeInstructions />
             </td>
           </tr>
           <tr>
-            <td className="py-6">
+            <td className="pt-8">
               <RecipeNutrition />
             </td>
           </tr>
