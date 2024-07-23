be0d21e25d13121b3d515fa7153715d7af73c104
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-07-23 04:46:51 -0400

    Add title component

--- app/components/recipe/components/ingredients/index.tsx
+++ app/components/recipe/components/ingredients/index.tsx
@@ -1,9 +1,10 @@
+import RecipeTitleLevel2 from "../title-level2"
+
 const RecipeIngredients = () => {
   return (
     <div className="flex flex-col gap-4">
-      <h3 className="text-fem-brown-800 text-3xl font-young-serif leading-none">
-        Ingredients
-      </h3>
+      <RecipeTitleLevel2>Ingredients</RecipeTitleLevel2>
+
       <ul className="flex flex-col gap-2 pl-6 list-disc marker:text-xs">
         <li className="pl-4">2-3 large eggs</li>
         <li className="pl-4">Salt, to taste</li>
