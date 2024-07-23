41ff53bb766ce22086ca0a10e5667d7309a2d04b
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-07-23 04:41:46 -0400

    Add attribution

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import HiddenH1 from "./components/hidden-h1"
 import Recipe from "./components/recipe"
+import Attribution from "./components/attribution"
 
 export default function Home() {
   return (
