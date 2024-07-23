886da0253f01788b52f31ec25c4f646311bc11c1
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-07-23 04:45:45 -0400

    Add relative image container

--- app/components/recipe/components/image/index.tsx
+++ app/components/recipe/components/image/index.tsx
@@ -3,6 +3,16 @@ import Image from "next/image"
 import recipeImage from "@/assets/images/image-omelette.jpeg"
 
 const RecipeImage = () => {
-  return <Image src={recipeImage} width={536} height={300} alt="recipe photo" />
+  return (
+    <div className="relative overflow-hidden sm:rounded-xl">
+      <Image
+        className="w-full h-full object-cover transition-all duration-500 group-hover:scale-110 group-hover:rotate-1"
+        src={recipeImage}
+        width={536}
+        height={300}
+        alt="recipe photo"
+      />
+    </div>
+  )
 }
 export default RecipeImage
